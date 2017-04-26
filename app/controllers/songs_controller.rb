3709b9e295e03e2075cae551256558ab3ea92775
Jon foster <redcabinny@gmail.com>
2017-04-26 09:12:45 -0400

    Improved controls on Wave Surfer. Implemented show route and json rendering for songs, and began AJAX process for getting songs and licesning information

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,9 @@
 class SongsController < ApplicationController
+	def show
+		@song = Song.find(params[:id])
+		respond_to do |format|
+			format.html { :show}
+			format.json { render json: @song}
+		end
+	end
 end
