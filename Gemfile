e8a67b25572aece330944603cdfb0ce38b1bd84f
Jon foster <redcabinny@gmail.com>
2017-04-26 09:47:15 -0400

    Added pagination

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
-
+gem 'will_paginate', '~> 3.1', '>= 3.1.5'
 gem 'active_model_serializers'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,7 +2,7 @@ class StoresController < ApplicationController
 
 	def index
 		MusicImporter.import  
-		@songs = Song.all
+		@songs = Song.all.paginate(:page => params[:page], per_page: 20)
 		respond_to do |format|
 			format.json { render json: Song.all }
 			format.html { render :index}
