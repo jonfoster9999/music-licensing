fecb876a0fc83408385ba5f907813ce2cbb18c5a
Jon foster <redcabinny@gmail.com>
2017-05-29 12:21:58 -0400

    no idea

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -1,6 +1,6 @@
 class ConverterController < ApplicationController
 	def convert 
-		@asset_path = ActionController::Base.helpers.asset_path(params[:string] + ".mp3")
+		@asset_path = ActionController::Base.helpers.asset_path(params[:string])
 		render plain: @asset_path
 	end
 end
